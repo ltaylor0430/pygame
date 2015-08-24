04de574adab2b7ad8418039cfa6b628d98441ffa
RedPillz <ltaylor0430@gmail.com>
2015-08-24 00:59:32 -0400

    Loaded a image on init and display it at x,y (0,0)

--- game.py
+++ game.py
@@ -8,6 +8,7 @@ deinitialize()
 """
 import pygame
 from pygame.locals import *
+from os.path import join
 
 
 class App:
@@ -15,7 +16,7 @@ class App:
     def __init__(self):
         self._running = True
         self._display_surf = None
-        self.size = self.weight, self.height = 640, 400
+        self.size = self.weight, self.height = 350, 350
 
     def on_init(self):
         # Create main display with hardware acceleration
@@ -23,12 +24,17 @@ class App:
         self._display_surf = pygame.display.set_mode(
           self.size, pygame.HWSURFACE | pygame.DOUBLEBUF)
         self._running = True
+        # Load Image
+        self._image_surf = pygame.image.load(
+          join("data/images", "hacker_symbol8x6.jpg")).convert()
 
     def on_loop(self):
         pass
 
     def on_render(self):
-        pass
+        # Update Screen
+        self._display_surf.blit(self._image_surf, (0, 0))
+        pygame.display.flip()  # Update the contents of the entire display
 
     def on_cleanup(self):
         # Before quiting, we will cleanup
@@ -46,8 +52,8 @@ class App:
         while(self._running):
             for event in pygame.event.get():
                 self.on_event(event)
-        self.on_loop()
-        self.on_render()
+            self.on_loop()
+            self.on_render()
         self.on_cleanup()
 
 if __name__ == '__main__':
