2475aee7485e9194f412f16ac6da8affb1ab4dd0
RedPillz <ltaylor0430@gmail.com>
2015-08-24 00:13:19 -0400

    Formatted code correctly

--- game.py
+++ game.py
@@ -9,40 +9,48 @@ deinitialize()
 import pygame
 from pygame.locals import *
 
+
 class App:
-  def __init__(self):
-    self._running = True
-    self._display_surf = None
-    self.size = self.weight , self.height = 640,400
-  def on_init(self):
-    """ Create main display with hardware acceleration """
 
-    pygame.init() #init pygame
-    self._display_surf = pygame.display.set_mode(self.size,pygame.HWSURFACE | pygame.DOUBLEBUF)
-    self._running = True
-  def on_loop(self):
-    pass
-  def on_render(self):
-    pass
-  def on_cleanup(self):
-    """ Before quiting, we will cleanup"""
-    pygame.quit()
-  def on_event(self,event):
-    #quit game event set flag to false to exit game loop
-    if event.type == pygame.QUIT:
-      self._running = False
-  def on_execute(self):
-    #call on_init()
-    if self.on_init() == False:
-      self._running = False
-    while( self._running ):
-      for event in pygame.event.get():
-          self.on_event(event)
-      self.on_loop()
-      self.on_render()
-    self.on_cleanup()
+    def __init__(self):
+        self._running = True
+        self._display_surf = None
+        self.size = self.weight, self.height = 640, 400
 
-if __name__=='__main__':
-  theApp = App()
-  #this function contains the game loop
-  theApp.on_execute()
+    def on_init(self):
+        # Create main display with hardware acceleration
+        pygame.init()  # Init pygame
+        self._display_surf = pygame.display.set_mode(
+          self.size, pygame.HWSURFACE | pygame.DOUBLEBUF)
+        self._running = True
+
+    def on_loop(self):
+        pass
+
+    def on_render(self):
+        pass
+
+    def on_cleanup(self):
+        # Before quiting, we will cleanup
+        pygame.quit()
+
+    def on_event(self, event):
+        # Quit game event set flag to false to exit game loop
+        if event.type == pygame.QUIT:
+            self._running = False
+
+    def on_execute(self):
+        # Call on_init()
+        if self.on_init() == False:
+            self._running = False
+        while(self._running):
+            for event in pygame.event.get():
+                self.on_event(event)
+        self.on_loop()
+        self.on_render()
+        self.on_cleanup()
+
+if __name__ == '__main__':
+    theApp = App()
+    # This function contains the game loop
+    theApp.on_execute()
